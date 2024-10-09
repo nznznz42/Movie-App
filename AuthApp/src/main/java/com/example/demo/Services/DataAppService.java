9bd99320620bd0467fc86baa2817700af1cc357f
nznznz42 <coolcarrot420@gmail.com>
2024-10-09 12:28:33 +0400

    minor fix

--- AuthApp/src/main/java/com/example/demo/Services/DataAppService.java
+++ AuthApp/src/main/java/com/example/demo/Services/DataAppService.java
@@ -5,11 +5,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.Map;
 
+@Service
 public class DataAppService {
 
     @Autowired
