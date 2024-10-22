80b897187ca2c3869876e39c2cf2459ca0b88db2
nznznz42 <coolcarrot420@gmail.com>
2024-10-22 08:57:28 +0400

    locked popular movies

--- DataApp/src/main/java/com/example/demo/Utils/JwtFilter.java
+++ DataApp/src/main/java/com/example/demo/Utils/JwtFilter.java
@@ -6,48 +6,49 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-public class JwtFilter extends GenericFilterBean {
+
+public class JwtFilter extends OncePerRequestFilter {
 
     @Autowired
     private SessionController sessionController;
 
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
-                         FilterChain filterChain)
-            throws IOException, ServletException {
-        HttpServletRequest request = (HttpServletRequest) servletRequest;
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
         String path = request.getRequestURI();
 
-        if (path.equals("/session/register")) {
-            filterChain.doFilter(servletRequest, servletResponse);
+        if (path.equals("/session/register") || path.startsWith("/search") || path.startsWith("/home") || path.startsWith("/movie")) {
+            filterChain.doFilter(request, response);
             return;
         }
 
         String authHeader = request.getHeader("Authorization");
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-            throw new ServletException("Missing or invalid Authorization header");
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Missing or invalid Authorization header");
+            return;
         } else {
             String token = authHeader.substring(7);
             if (!sessionController.getTokenStore().contains(token)) {
-                throw new ServletException("Unauthorized: Token not registered");
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized: Token not registered");
+                return;
             }
             Claims claims = Jwts.parser().setSigningKey("AuthSecret").parseClaimsJws(token).getBody();
             String emailId = (String) claims.get("currentUserEmailId");
             String name = (String) claims.get("currentUserName");
 
-
             request.setAttribute("emailId", emailId);
             request.setAttribute("userName", name);
-            filterChain.doFilter(request, servletResponse);
+            filterChain.doFilter(request, response);
         }
     }
 }
+
 
